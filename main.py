079df06d16330032d7b3982dd8dfeabc226891a2
Archit Kothari <98614998+akothar2@users.noreply.github.com>
2022-02-08 22:41:02 +0000

    finished part a

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
-
 #Part A
 weeks = 16
 classes = 5
 tuition = 6000
 cost_per_week = ((tuition / classes) / weeks)
 print("Cost per week:", cost_per_week)
-
+classes_per_week = 3
+cost_per_class = (cost_per_week / classes_per_week)
+print("Cost per class:", cost_per_class)
+print("Weeks", type(weeks))
+print("Classes", type(classes))
+print("Tuition", type(tuition))
+print("Cost per week", type(cost_per_week))
+print("Classes per week", type(classes_per_week))
+print("Cost per class", type(cost_per_class))
 
 #Part B

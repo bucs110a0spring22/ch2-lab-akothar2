35eb525b23a596318b02b575fe0cf61eee3b7458
Archit Kothari <98614998+akothar2@users.noreply.github.com>
2022-02-14 19:58:20 +0000

    edited style

--- main.py
+++ main.py
@@ -2,19 +2,24 @@ import random
 
 #Part A
 weeks = 16
+print("Weeks:", weeks, type(weeks))
+
 classes = 5
+print("Classes:", classes, type(classes))
+
 tuition = 6000
+print("Tuition:", tuition, type(tuition))
+
 cost_per_week = ((tuition / classes) / weeks)
+print("Cost per week:", cost_per_week, type(cost_per_week))
 print("Cost per week:", cost_per_week)
+
 classes_per_week = 3
+print("Classes per week:", classes_per_week, type(classes_per_week))
+
 cost_per_class = (cost_per_week / classes_per_week)
-print("Cost per class:", cost_per_class)
-print("Weeks", type(weeks))
-print("Classes", type(classes))
-print("Tuition", type(tuition))
-print("Cost per week", type(cost_per_week))
-print("Classes per week", type(classes_per_week))
-print("Cost per class", type(cost_per_class))
+print("Cost per class:", cost_per_class, type(cost_per_class))
+print("Cost per class is", cost_per_class)
 
 #Part B
 print(random.randrange(1,10))
